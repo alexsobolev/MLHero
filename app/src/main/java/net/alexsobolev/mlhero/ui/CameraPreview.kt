b2815d82f596827a65067da4c17e196be51ed012
Aleksandr Sobolev <aleksandr.sobolev@axelspringer.com>
2023-01-30 02:12:46 +0100

    Step 6: Implement Selfie-Segmentation  - Add dependencies  - Add Segmenter  - Show mask on the preview

--- app/src/main/java/net/alexsobolev/mlhero/ui/CameraPreview.kt
+++ app/src/main/java/net/alexsobolev/mlhero/ui/CameraPreview.kt
@@ -3,6 +3,7 @@ package net.alexsobolev.mlhero.ui
 import android.util.Size
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.mlkit.vision.MlKitAnalyzer
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
@@ -25,10 +26,13 @@ import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetector
 import com.google.mlkit.vision.face.FaceDetectorOptions
-import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseDetection
 import com.google.mlkit.vision.pose.PoseDetector
 import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
+import com.google.mlkit.vision.segmentation.Segmentation
+import com.google.mlkit.vision.segmentation.SegmentationMask
+import com.google.mlkit.vision.segmentation.Segmenter
+import com.google.mlkit.vision.segmentation.selfie.SelfieSegmenterOptions
 
 @Composable
 fun CameraPreview(
@@ -50,11 +54,13 @@ fun CameraPreview(
 
 //    var barcodes by remember { mutableStateOf<List<Barcode>>(emptyList()) }
 //    var faces by remember { mutableStateOf<List<Face>>(emptyList()) }
-    var pose by remember { mutableStateOf<Pose?>(null) }
+//    var pose by remember { mutableStateOf<Pose?>(null) }
+    var mask by remember { mutableStateOf<SegmentationMask?>(null) }
 
 //    val barcodeScanner = buildBarcodeScanner()
 //    val faceDetector = buildFaceDetector()
-    val poseDetector = buildPoseDetector()
+//    val poseDetector = buildPoseDetector()
+    val segmenter = buildSegmenter()
 
     val cameraController = remember(camera) {
         LifecycleCameraController(context).apply {
@@ -69,9 +75,10 @@ fun CameraPreview(
                     listOf(
 //                        barcodeScanner,
 //                        faceDetector,
-                        poseDetector
+//                        poseDetector,
+                        segmenter
                     ),
-                    CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED,
+                    ImageAnalysis.COORDINATE_SYSTEM_ORIGINAL,
                     ContextCompat.getMainExecutor(context)
                 ) { result ->
                     /*result?.getValue(barcodeScanner)?.let { detectedBarcodes ->
@@ -80,8 +87,11 @@ fun CameraPreview(
                     /*result?.getValue(faceDetector)?.let { detectedFaces ->
                         faces = detectedFaces
                     }*/
-                    result?.getValue(poseDetector)?.let { detectedPose ->
+                    /*result?.getValue(poseDetector)?.let { detectedPose ->
                         pose = detectedPose
+                    }*/
+                    result?.getValue(segmenter)?.let { detectedMask ->
+                        mask = detectedMask
                     }
                 }
             )
@@ -97,8 +107,13 @@ fun CameraPreview(
 
 //    Barcodes(barcodes = barcodes)
 //    Faces(faces = faces)
-    Pose(
+    /*Pose(
         pose = pose,
+        resolution = cameraController.imageAnalysisTargetSize?.resolution,
+        isCameraMirrored = camera == CameraSelector.LENS_FACING_FRONT
+    )*/
+    SegmentationMaskPreview(
+        segmentationMask = mask,
         resolution = cameraController.imageAnalysisTargetSize?.resolution,
         isCameraMirrored = camera == CameraSelector.LENS_FACING_FRONT
     )
@@ -128,6 +143,13 @@ private fun buildPoseDetector(): PoseDetector {
         .build()
     return PoseDetection.getClient(poseDetectorOptions)
 }
+
+private fun buildSegmenter(): Segmenter =
+    Segmentation.getClient(
+        SelfieSegmenterOptions.Builder()
+            .setDetectorMode(SelfieSegmenterOptions.STREAM_MODE)
+            .build()
+    )
 
 private val IMAGE_ANALYSIS_TARGET_SIZE = Size(480, 640)
 private const val MIN_FACE_SIZE = 0.2f
